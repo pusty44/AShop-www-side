f32c0de4f57c46a77825702f36271b1a663111ff
Dawid <d.pierzak@itkreatywni.pl>
2019-03-17 11:02:24 +0100

    templates for faq, vouchers and page

--- src/Controller/shop/homePageController.php
+++ src/Controller/shop/homePageController.php
@@ -37,4 +37,75 @@ class homePageController extends AbstractController
             'breadcrumbs' => $breadcrumbs
         ]);
     }
+
+    /** DELETE CONTROLLERS UNDER THIS COMMENT **/
+
+    /**
+     * Get common of AShop
+     *
+     * @Route("/faq", name="faq")
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function faq()
+    {
+        $servicesRepo = $this->getDoctrine()->getRepository(Services::class);
+        $serversRepo = $this->getDoctrine()->getRepository(Servers::class);
+        $services = $servicesRepo->findAll();
+        $servers = $serversRepo->findAll();
+        $breadcrumbs = [];
+
+        return $this->render('frontend/pages/faq/index.html.twig', [
+            'services' => $services,
+            'servers' => $servers,
+            'title' => 'Często zadawane pytania',
+            'breadcrumbs' => $breadcrumbs
+        ]);
+    }
+
+    /**
+     * Get common of AShop
+     *
+     * @Route("/voucher", name="voucher")
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function voucher()
+    {
+        $servicesRepo = $this->getDoctrine()->getRepository(Services::class);
+        $serversRepo = $this->getDoctrine()->getRepository(Servers::class);
+        $services = $servicesRepo->findAll();
+        $servers = $serversRepo->findAll();
+        $breadcrumbs = [];
+
+        return $this->render('frontend/pages/vouchers/index.html.twig', [
+            'services' => $services,
+            'servers' => $servers,
+            'title' => 'Wykorzystaj voucher',
+            'breadcrumbs' => $breadcrumbs
+        ]);
+    }
+
+    /**
+     * Get common of AShop
+     *
+     * @Route("/default-page", name="default-page")
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function defaulpage()
+    {
+        $servicesRepo = $this->getDoctrine()->getRepository(Services::class);
+        $serversRepo = $this->getDoctrine()->getRepository(Servers::class);
+        $services = $servicesRepo->findAll();
+        $servers = $serversRepo->findAll();
+        $breadcrumbs = [];
+
+        return $this->render('frontend/pages/default-page/index.html.twig', [
+            'services' => $services,
+            'servers' => $servers,
+            'title' => 'Wykorzystaj voucher',
+            'breadcrumbs' => $breadcrumbs
+        ]);
+    }
 }
